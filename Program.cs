be65b1670c17db2d88a550defee46f7e5e1792ce
alexisonwire <47860770+alexisonwire@users.noreply.github.com>
2024-04-21 18:13:24 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LinuxWebJobs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+
+                var now = DateTime.Now;
+                Console.WriteLine($"{now}");
+            }
+        }
+    }
+}
